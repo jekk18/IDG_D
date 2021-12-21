69ed09f7ac6b1ee40c9bbed17ffae40a50bde1c9
Jeko Tchelidze <jekotchelidze@gmail.com>
2021-12-21 13:57:27 +0400

    c

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+  document.getElementById('first-button').click();
+}
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "inline-block";
+  evt.currentTarget.className += " active";
+}
+
+$(document).ready(function(){
+  $('.team-slider').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows:false,
+  });
+});
+
+$('.burger-lines').on('click', function(){
+$('.burger-menu').toggleClass('active');
+});
+
+$(window).scroll(function(){
+  var headSticky = $('.header'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 50) headSticky.addClass('addShadow');
+  else headSticky.removeClass('addShadow');
+});
